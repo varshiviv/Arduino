90ce39183b486e2071941f9cb4af0c30fc4ce832
Vivek Sharanappa <vivek.hs@gmail.com>
2016-08-12 21:50:38 +0530

    Tweet from edison
    using grove kit

---
+++ Tweet_frm_Edison/Tweet_frm_Edison.ino
@@ -0,0 +1,31 @@
+#include <SPI.h>
+#include <Ethernet.h>
+
+#include <Twitter.h>
+
+void setup(){
+    pinMode(7, INPUT);
+}
+
+void loop(){
+
+    if(digitalRead(7)){
+        tweetMessage();
+        delay(1000);
+    }
+}
+
+void tweetMessage(){
+    Twitter twitter("122389426-X1UImGRsnRyjsUISCLM9HVd1OyHaytcxx7jJQElK");
+    //Our message (in lolcat, of course)
+    String stringMsg = "All ur lightz be ";
+    stringMsg += analogRead(0);
+    stringMsg += " out of 1023. #tweet #from #edison #board #IoT";
+
+    //Convert our message to a character array
+    char msg[140];
+    stringMsg.toCharArray(msg, 140);
+
+    //Tweet that sucker!
+    twitter.post(msg);
+}
